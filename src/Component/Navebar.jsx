be0ffe42f154e79c7b314603a701b804bc7c90bc
hamza-elmoudden <hamzaelmouddane2000@gmail.com>
2024-05-19 06:01:27 +0100

    change naverbar madia

--- src/Component/Navebar.jsx
+++ src/Component/Navebar.jsx
@@ -3,16 +3,19 @@ import { faDiceD20,faBars } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
+
+
 export const Navebar = () => {
     
     const showbar = ()=>{
-        const bar = document.querySelector('.navh')
+        const bar = document.querySelector('.lefbard')
         bar.classList.toggle("sh")
     }
 
+
   return (
     <>
-    <motion.header animate={{top:0,}} transition={{ duration: 0.5 }} className="py-10  bg-black text-white text-end px-8 md:px-20 md:relative fixed w-full z-30 top-[-10rem]">
+    <motion.header animate={{top:0,}} transition={{ duration: 0.5 }} className="md:py-10 py-5   bg-black text-white text-end px-8 md:px-20 md:relative fixed w-full z-30 top-[-10rem]">
         <nav className=" hidden md:flex items-center justify-evenly container mx-auto">
                 <Link className="text-4xl font-bold cursor-pointer uppercase" to="/">
                     rolex
@@ -38,12 +41,12 @@ export const Navebar = () => {
         <div className="text-4xl cursor-pointer block md:hidden" onClick={showbar}>
                     <FontAwesomeIcon icon={faBars} />
         </div>
-        <div className=" bg-black navh w-80 flex flex-col z-30 -right-96 top-28 py-8 px-4 md:hidden fixed">
+        <div className=" bg-black lefbard   w-80 flex flex-col z-30 -right-96 top-[80px] py-8 px-4 md:hidden fixed h-[100vh]">
             <nav className="flex flex-col-reverse gap-10  text-center">
             <Link className="text-4xl font-bold cursor-pointer uppercase" to="/">
                     rolex
                 </Link>
-                <div className="flex gap-20 flex-col items-center">
+                <div className="flex gap-10 flex-col items-center">
                     <div className="flex gap-8">
                         <Link className="text-2xl capitalize cursor-pointer" to="/">Home</Link>
                     </div>
